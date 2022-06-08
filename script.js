1a1a7dcfccd67336780894cc07a7797ea211dd51
Ryan <ryanling521@g.ucla.edu>
2022-06-08 13:10:38 -0700

    added the hovering effects

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ for (let i = 1; i < 17; i++) {
     hi.style.borderColor = 'gray';
     container.append(hi);
 }
+
+const divs = Array.from(document.querySelectorAll('.divs'));
+
+// function removeBackground(e) {
+//     e.target.style.backgroundColor = "purple";
+//     if (!e.target) {
+//         e.target.style.backgroundColor = "white";
+//     }
+//   }
+
+// divs.forEach(div => div.addEventListener('mouseover', e => e.target.style.backgroundColor = "purple"));
+divs.forEach(function(div) {
+  div.addEventListener('mouseenter', e => e.target.style.backgroundColor = 'purple');
+  div.addEventListener('mouseleave', e => e.target.style.backgroundColor = 'white');
+});
