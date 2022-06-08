925a05fe4b1c0585a0520a570e4cc4a5f98a6be8
Ryan <ryanling521@g.ucla.edu>
2022-06-08 15:06:33 -0700

    now can adjust to any inputted number of rows/columns

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
+// large container for the 16 grids
 const body = document.body;
 const container = document.querySelector('.container');
-// large container for the 16 grids
 
-// grids
-for (let i = 1; i < 17; i++) {
-    let hi = document.createElement('div');
-    hi.textContent = `${i}`;
-    hi.classList.add('divs');   
-    hi.style.borderColor = 'gray';
-    container.append(hi);
+// function to remove children
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function populate(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 1; i < size*size+1; i++) {
+        let square = document.createElement('div');
+        square.classList.add('divs');
+        square.style.backgroundColor = 'white';
+        square.style.borderColor = 'black';
+        square.textContent = `${i}`;
+        container.append(square);
+    }
 }
 
-const divs = Array.from(document.querySelectorAll('.divs'));
+// take input from button
+const btn = document.querySelector('#btn');
+btn.addEventListener('click',function() {
+    // remove previous elements
+    removeAllChildNodes(container);
 
-// function removeBackground(e) {
-//     e.target.style.backgroundColor = "purple";
-//     if (!e.target) {
-//         e.target.style.backgroundColor = "white";
-//     }
-//   }
+    // // make divs
+    let numberofSquares = document.getElementById("squares").value;
+    populate(numberofSquares);
 
-// divs.forEach(div => div.addEventListener('mouseover', e => e.target.style.backgroundColor = "purple"));
-divs.forEach(function(div) {
-  div.addEventListener('mouseenter', e => e.target.style.backgroundColor = 'purple');
-  div.addEventListener('mouseleave', e => e.target.style.backgroundColor = 'white');
+    // select all divs 
+    const divs = Array.from(document.querySelectorAll('.divs'));
+    
+    // add hovering effect to all divs
+    divs.forEach(function(div) {
+      div.addEventListener('mouseenter', e => e.target.style.backgroundColor = 'purple');
+      div.addEventListener('mouseleave', e => e.target.style.backgroundColor = 'white');
+    });
 });
+
+
+
+
+
